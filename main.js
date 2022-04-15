ae57e4d4ee67369096e97901284f53eb7f5af9ee
Ninad-075 <80702995+Ninad-075@users.noreply.github.com>
2022-04-15 16:56:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 song = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
-    song = loadSound("Avicii - The Nights(1).mp3")
+    song = loadSound("Avicii - The Nights (1).mp3")
     song2 = loadSound("Against the Current - Legends Never Die.mp3")
 
 }
@@ -13,6 +17,13 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized')
 }
 
 function draw(){
@@ -22,4 +33,23 @@ function draw(){
 function play()
 {
     song.play();
+    song2.play();
+    song.setVolume(1);
+    song2.setVolume(0.9);
+    song.rate(1);
+    song.rate(1.25);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+    }
 }
